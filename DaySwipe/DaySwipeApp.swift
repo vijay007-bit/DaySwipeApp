2cf40126b18b3912650a1d5d0f5975bfb343612d
Vijay Vikram Singh <svijayvikram00@gmail.com>
2023-06-25 21:31:54 +0530

    added missing files

---
+++ DaySwipe/DaySwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaySwipeApp.swift
+//  DaySwipe
+//
+//  Created by Vijay Singh on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct DaySwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
